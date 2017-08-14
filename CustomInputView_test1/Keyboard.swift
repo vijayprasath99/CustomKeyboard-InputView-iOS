13e3e84b96921a3681e174f0dac7948ab411fd27
vijayprasath99 <vijayprasath99@gmail.com>
2017-08-14 11:44:10 -0400

    Add UIScrollView with Page Navigator Buttons

--- CustomInputView_test1/Keyboard.swift
+++ CustomInputView_test1/Keyboard.swift
@@ -16,6 +16,14 @@ protocol KeyboardDelegate: class {
 
 class Keyboard: UIView {
     
+     let initialSubview = "Keyboard"
+    
+    var keyboardPlaceHolderView : UIView!
+    var scrollView : UIScrollView!
+    var viewsToAdd = [UIView]()
+    var contentOffSetArray = [CGFloat]()
+    var contentSize : CGFloat = 0.0
+    
     // This variable will be set as the view controller so that
     // the keyboard can send messages to the view controller.
     weak var delegate: KeyboardDelegate?
@@ -24,27 +32,64 @@ class Keyboard: UIView {
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        initializeSubviews()
+        initializeSubviews(withXibFileName: initialSubview)
+        initializeScrollView()
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        initializeSubviews()
+        initializeSubviews(withXibFileName: initialSubview)
+        initializeScrollView()
     }
     
-    func initializeSubviews() {
-        let xibFileName = "Letters" // xib extention not included
+    func initializeSubviews(withXibFileName subviewName : String) {
+        //MAIN KEYBOARD VIEW
+        let xibFileName = subviewName
         let view = Bundle.main.loadNibNamed(xibFileName, owner: self, options: nil)?[0] as! UIView
         self.addSubview(view)
         view.frame = self.bounds
+        
+        //LETTERS SUBVIEW
+        let xibFileName2 = "Letters"
+        let view2 = Bundle.main.loadNibNamed(xibFileName2, owner: self, options: nil)?[0] as! UIView
+        
+        //NUMBERS SUBVIEW
+        let xibFileName3 = "Numbers"
+        let view3 = Bundle.main.loadNibNamed(xibFileName3, owner: self, options: nil)?[0] as! UIView
+        
+        keyboardPlaceHolderView = view
+        scrollView = view.viewWithTag(1001) as! UIScrollView
+        viewsToAdd.append(view2)
+        viewsToAdd.append(view3)
     }
     
-    // MARK:- Button actions from .xib file
+    func initializeScrollView(){
+        for i in 0..<viewsToAdd.count {
+            let currentViewToAdd = viewsToAdd[i]
+            scrollView.addSubview(currentViewToAdd)
+            
+            let newX : CGFloat = currentViewToAdd.frame.width * CGFloat(i)
+            contentOffSetArray.append(newX)
+            contentSize += currentViewToAdd.frame.width
+            
+            currentViewToAdd.frame = CGRect(x: newX, y: 0, width: currentViewToAdd.frame.width, height: currentViewToAdd.frame.height)
+        }
+        scrollView.contentSize = CGSize(width: contentSize, height: scrollView.frame.height)
+    }
     
+    
+    
+    // MARK:- Button actions from .xib file
     @IBAction func keyTapped(sender: UIButton) {
         // When a button is tapped, send that information to the
         // delegate (ie, the view controller)
         self.delegate?.keyWasTapped(character: sender.titleLabel!.text!) // could alternatively send a tag value
     }
     
+    // MARK:- Button to initiate Transition between pages
+    @IBAction func transitionInitiated(sender: UIButton) {
+        var contentOffSet = scrollView.contentOffset
+        contentOffSet.x = contentOffSetArray[sender.tag]
+        scrollView.setContentOffset(contentOffSet, animated: true)
+    }
 }
